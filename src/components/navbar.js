4d57a005320778806c6bc77cc942ea711f43b0c1
Patricio Ferraggi <pattferraggi@gmail.com>
2019-07-15 23:01:08 +0200

    cleaning assets, setting technologies

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,18 @@
 import React from "react"
 
-export default () => <div className="navbar__container">NAVBAR</div>
+export default () => (
+  <div className="navbar__container">
+    <div className="navbar__container__title">patricio</div>
+    <div className="navbar__container__title">ferraggi ares</div>
+    <div className="navbar__container__subtitle">fullstack dev</div>
+    <ul className="navbar__container__links__container">
+      <li className="navbar__container__links__container__link-active">
+        intro
+      </li>
+      <li>blog</li>
+      <li>about</li>
+      <li>projects</li>
+      <li>contact</li>
+    </ul>
+  </div>
+)
