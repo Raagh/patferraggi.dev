114ed14e20507ca288137ee9441a53371ce4393b
Patricio Ferraggi <pattferraggi@gmail.com>
2019-07-16 22:58:25 +0200

    added external links to nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,6 @@
 import React from "react"
+import GithubIcon from "../assets/icons/github.png"
+import LinkedinIcon from "../assets/icons/linkedin.png"
 
 export default () => (
   <ul className="navbar">
@@ -7,5 +9,19 @@ export default () => (
     <li>about</li>
     <li>projects</li>
     <li>contact</li>
+    <li className="navbar__link--active navbar__link--separated">eng</li>
+    <li>esp</li>
+    <li className="navbar__link--separated-extra">
+      <a href="https://www.linkedin.com/in/patricio-ferraggi-ares/">
+        <img
+          className="link--external"
+          alt="linkedin link"
+          src={LinkedinIcon}
+        />
+      </a>
+      <a href="https://github.com/Raagh/">
+        <img className="link--external" alt="github link" src={GithubIcon} />
+      </a>
+    </li>
   </ul>
 )
