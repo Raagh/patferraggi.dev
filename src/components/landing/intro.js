f801acd25dfd41297d521eadf1062aa185f1167c
Patricio Ferraggi <pattferraggi@gmail.com>
2019-07-31 22:58:29 +0200

    refactor sass

--- src/components/landing/intro.js
+++ src/components/landing/intro.js
@@ -2,10 +2,12 @@ import React from "react"
 
 export default () => (
   <div className="intro">
-    <p>
-      I am an Argentine software developer, currently based in Belgium. I am
-      passionate about software craftmanship and creating high quality solutions
-      that make people happy.
+    <p className="intro__text">
+      I am an Argentine software developer, currently based in Belgium.
+    </p>
+    <p className="intro__text">
+      I am passionate about software craftmanship and creating high quality
+      solutions that makes people happy.
     </p>
   </div>
 )
