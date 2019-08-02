3b9eed29cee054091e41f11d404b788cd1daedd3
Patricio Ferraggi <pattferraggi@gmail.com>
2019-08-02 23:50:32 +0200

    started adding blog news

---
+++ src/components/landing/blog-news.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default () => (
+  <div className="blog-news">
+    <p>I also have a blog, you can read</p>
+    <p>
+      it{" "}
+      <a className="blog-news__link" href="/blog">
+        here
+      </a>
+      , and these are the latest
+    </p>
+    <p>articles:</p>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
 import Intro from "../components/landing/intro"
+import BlogNews from "../components/landing/blog-news"
 import "../_sass/external/reset.scss"
 import "../_sass/index.scss"
 
 export default () => (
   <Layout>
     <Intro></Intro>
+    <BlogNews></BlogNews>
   </Layout>
 )
