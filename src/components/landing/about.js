9b18b76b4193bfdb86d509e9fcb0bb319fd9569e
Patricio Ferraggi <pattferraggi@gmail.com>
2019-08-11 16:57:33 +0200

    about me section

--- src/components/landing/about.js
+++ src/components/landing/about.js
@@ -2,22 +2,41 @@ import React from "react"
 
 export default () => (
   <section className="about-container">
-    <div className="background__title">So about me!, What's my background</div>
-    <p className="background__text">
-      Over the last 4 years, I have created of great variety high quality web
-      business applications using technologies like C#, asp.net core, HTML5,
-      CSS3, Angular, Javascript and Typescript. <br />I focus on creating high
-      quality software that is testable, maintainable, reusable and adaptive
-      that is required to succeed in Agile environments.
-    </p>
+    <div className="background__title">
+      So <span className="text--isSecondaryColor">about me!</span>, what's my
+      background
+    </div>
+    <article className="background__text">
+      <p>
+        Over the last 5 years, I have created of great variety high quality web
+        business applications using technologies like C#, asp.net core, HTML5,
+        CSS3, Angular, Javascript and Typescript. <br />I focus on creating high
+        quality software that is testable, maintainable, reusable and adaptive
+        that is required to succeed in Agile environments.
+      </p>
+      <p>
+        I've have experience in startup, medium and large size entreprises and I
+        am used to working on teams but also managing my own projects. <br />
+        Some of the companies I've worked for include: Rydoo, Telefonica
+        Argentina, Cencosud and YPF. Check my LinkedIn for more details.
+      </p>
+      <p>
+        As an employee I am proactive, detail oriented and I enjoy playing an
+        active role in improving business.
+      </p>
+    </article>
 
-    <div className="extra__title">Apart from code...</div>
-    <p className="extra__text">
-      I've have experience in startup, medium and large size entreprises and I
-      am used to working on teams but also managing my own projects. Some of the
-      companies I've worked for include Telefonica Argentina, Cencosud and YPF.
-      Checm my Linked in for more details. As an employee I am proactive, detail
-      oriented and I enjoy playing an active role in improving business.
-    </p>
+    <div className="extra__title">Apart from work...</div>
+    <article className="extra__text">
+      <p>
+        I love writting on my blog, creating software development courses,
+        giving coaching to other developers <br /> and experimenting with
+        multiple technologies on my open source projects.
+      </p>
+      <p>
+        I exercise often and I have been practicing Muay Thai for the last 7
+        years.
+      </p>
+    </article>
   </section>
 )
