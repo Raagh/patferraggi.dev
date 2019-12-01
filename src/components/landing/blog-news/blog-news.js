eecedf8b3d7e32935b42e47fdce1bbd53095197e
Patricio Ferraggi <pattferraggi@gmail.com>
2019-12-01 22:10:13 +0100

    transform blog-news to styled components

--- src/components/landing/blog-news/blog-news.js
+++ src/components/landing/blog-news/blog-news.js
@@ -1,5 +1,72 @@
 import React from "react"
 import Article from "./article"
+import styled from "styled-components"
+import device from "../../shared/device"
+import globalStyles from "../../shared/global-styles"
+
+const BlogNewsWrapper = styled.div`
+  grid-area: blog-news;
+  font-size: 64px;
+  font-family: ${globalStyles.fontFamilyMedium};
+  line-height: 64px;
+  padding-top: 1.5rem;
+
+  @media ${device.phone} {
+    font-size: 32px;
+    line-height: 32px;
+  }
+`
+
+const BlogNewsText = styled.p`
+  width: 90%;
+
+  @media ${device.phone} {
+    width: 100%;
+  }
+`
+
+const BlogNewsLink = styled.a`
+  color: ${globalStyles.secondaryColor};
+`
+
+const BlogNewsArticles = styled.section`
+  padding-top: 2rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 100%;
+
+  @media ${device.phone} {
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export default () => (
+  <BlogNewsWrapper id="blog">
+    <BlogNewsText>
+      I also have a blog,
+      <BlogNewsLink href="/blog"> read it here</BlogNewsLink>
+    </BlogNewsText>
+    <BlogNewsArticles>
+      {articles.map(article => {
+        const articleIndex = articles.indexOf(article)
+        const isLastElement = true
+
+        return (
+          <Article
+            key={articleIndex}
+            title={article.title}
+            link={article.link}
+            isActive={articleIndex === 0}
+            creationDate={article.creationDate}
+            addDivider={isLastElement}
+          />
+        )
+      })}
+    </BlogNewsArticles>
+  </BlogNewsWrapper>
+)
 
 const articles = [
   {
@@ -22,32 +89,3 @@ const articles = [
       "https://medium.com/@patferraggi/how-i-found-a-problem-with-angular-unit-testing-and-decided-to-fix-it-myself-7c00b58d57a7",
   },
 ]
-
-export default () => (
-  <div id="blog" className="blog-news">
-    <p className="blog-news__text">
-      I also have a blog,
-      <a className="blog-news__link" href="/blog">
-        {" "}
-        read it here
-      </a>
-    </p>
-    <section className="blog-news__articles">
-      {articles.map(article => {
-        const articleIndex = articles.indexOf(article)
-        const isLastElement = true
-
-        return (
-          <Article
-            key={articleIndex}
-            title={article.title}
-            link={article.link}
-            isActive={articleIndex === 0}
-            creationDate={article.creationDate}
-            addDivider={isLastElement}
-          />
-        )
-      })}
-    </section>
-  </div>
-)
---
+++ src/components/shared/global-styles.js
@@ -0,0 +1,17 @@
+const black = "#0b0b0e"
+const blue = "#009bff"
+const white = "#ffffff"
+const grey = "#707070"
+const interMedium = "Inter Medium"
+const interRegular = "Inter Regular"
+
+export default {
+  backgroundColor: black,
+  primaryColor: white,
+  secondaryColor: blue,
+  inactiveColor: grey,
+  fontFamilyMedium: interMedium,
+  fontFamilyRegular: interRegular,
+  externalPadding: "5rem",
+  internalPadding: "3rem",
+}
