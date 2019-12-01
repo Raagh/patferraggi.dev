d14752222709e14f233bf610b2c182bf6f910602
Patricio Ferraggi <pattferraggi@gmail.com>
2019-12-01 22:40:01 +0100

    transfrom projects to styled components

--- src/components/landing/projects/projects.js
+++ src/components/landing/projects/projects.js
@@ -1,5 +1,53 @@
 import React from "react"
 import Project from "./project"
+import device from "../../shared/device"
+import styled from "styled-components"
+import globalStyles from "../../shared/global-styles"
+
+const ProjectsWrapper = styled.section`
+  grid-area: projects;
+  padding-top: 5rem;
+
+  @media ${device.phone} {
+    padding-top: 3rem;
+  }
+`
+
+const ProjectsWrapperText = styled.p`
+  padding-bottom: 4rem;
+  font-size: 64px;
+  font-family: ${globalStyles.fontFamilyMedium};
+  line-height: 64px;
+
+  .isSecondaryColor {
+    color: ${globalStyles.secondaryColor};
+  }
+
+  @media ${device.phone} {
+    padding-bottom: 2rem;
+    line-height: 32px;
+    font-size: 32px;
+  }
+`
+
+export default () => (
+  <ProjectsWrapper id="projects">
+    <ProjectsWrapperText>
+      Some examples of my <span className="isSecondaryColor">work</span>
+    </ProjectsWrapperText>
+    {projects.map(project => {
+      const projectIndex = projects.indexOf(project)
+
+      return (
+        <Project
+          key={projectIndex}
+          name={project.name}
+          skills={project.skills}
+        />
+      )
+    })}
+  </ProjectsWrapper>
+)
 
 const projects = [
   {
@@ -17,23 +65,3 @@ const projects = [
     ],
   },
 ]
-
-export default () => (
-  <section id="projects" className="projects-container">
-    <p className="projects-container__text">
-      Some examples of my{" "}
-      <span className="projects-container__text--isSecondaryColor">work</span>
-    </p>
-    {projects.map(project => {
-      const projectIndex = projects.indexOf(project)
-
-      return (
-        <Project
-          key={projectIndex}
-          name={project.name}
-          skills={project.skills}
-        />
-      )
-    })}
-  </section>
-)
--- src/components/shared/device.js
+++ src/components/shared/device.js
@@ -1,17 +1,8 @@
-const size = {
-  phone: "667px",
-  tablet: "768px",
-  tabletPortrait: "(min-width: 736px) and (max-width: 1366px)",
-  laptop: "1024px",
-  laptopL: "1440px",
-  desktop: "2560px",
-}
-
 export default {
-  phone: `(max-width: ${size.phone})`,
-  tablet: `(min-width: ${size.tablet})`,
-  tabletPortrait: size.tabletPortrait,
-  laptop: `(min-width: ${size.laptop})`,
-  laptopL: `(min-width: ${size.laptopL})`,
-  desktop: `(min-width: ${size.desktop})`,
+  phone: `(max-width: 667px)`,
+  tablet: `(min-width: 768px)`,
+  tabletPortrait: `(min-width: 736px) and (max-width: 1366px)`,
+  laptop: `(min-width: 1024px)`,
+  laptopL: `(min-width: 1440px)`,
+  desktop: `(min-width: 2560px)`,
 }
