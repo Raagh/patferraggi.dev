e34efd0825d4847fad0775af306c0f7784f1b936
Patricio Ferraggi <pattferraggi@gmail.com>
2020-01-16 11:54:00 +0100

    fix blog home linking

--- src/components/blog/layout.js
+++ src/components/blog/layout.js
@@ -11,57 +11,27 @@ class Layout extends React.Component {
       .split("/")
       .filter(Boolean)
       .pop()
-    const isPaginatedPath = pageNumber && Boolean(pageNumber.match(/^[0-9]+$/))
-    const rootPath = `${__PATH_PREFIX__}/`
-    const blogPath = `${__PATH_PREFIX__}/blog/`
-    let header
-
-    if (
-      location.pathname === rootPath ||
-      location.pathname === blogPath ||
-      isPaginatedPath
-    ) {
-      header = (
-        <h1
+    const header = (
+      <h3
+        style={{
+          ...scale(0.7),
+          marginBottom: rhythm(1.5),
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
           style={{
-            ...scale(1),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
           }}
+          to={`/blog/`}
         >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={location.pathname === blogPath ? `/blog/` : `/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/blog/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+          {title}
+        </Link>
+      </h3>
+    )
     return (
       <Wrapper>
         <div
