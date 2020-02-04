0c68c30772b6851ae4432b62477469b9aa9c8b15
Patricio Ferraggi <pattferraggi@gmail.com>
2020-02-04 12:01:14 +0100

    basic about

--- src/components/landing/about.js
+++ src/components/landing/about.js
@@ -1,136 +1,110 @@
 import React from "react"
 import styled from "styled-components"
-import device from "../shared/device"
-import globalStyles from "../shared/global-styles"
+import device from "../../config/device"
+import globalStyles from "../../config/style-variables"
 
 const AboutWrapper = styled.section`
-  grid-area: about;
-  padding-top: 5rem;
-  display: grid;
-  z-index: 1;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-areas:
-    "back-title back-text"
-    "back-title back-text"
-    "back-title back-text"
-    "extra-title extra-text";
+  display: flex;
+  flex-direction: row;
+  font-family: ${globalStyles.fontFamilyMedium};
+  font-style: normal;
+  font-weight: 500;
+  color: ${globalStyles.primaryColor};
+  margin: 7rem 0 5rem 0;
 
-  @media ${device.phone} {
-    grid-area: about;
-    padding-top: 5rem;
-    display: flex;
+  @media ${device.small} {
     flex-direction: column;
+    margin: 2rem 0 2rem 0;
+    padding: 0.5rem;
+  }
+
+  @media ${device.medium} {
+    margin: 2rem 0 2rem 0;
+    padding: 0.5rem;
   }
 `
 const BackgroundTitle = styled.div`
-  grid-area: back-title;
-  font-family: ${globalStyles.fontFamilyMedium};
+  font-size: 32px;
   line-height: 64px;
-  font-size: 64px;
-  padding-right: 20px;
+  letter-spacing: -2.2528px;
+  width: 50%;
 
-  @media ${device.phone} {
-    padding-bottom: 1rem;
-    line-height: 32px;
-    font-size: 32px;
+  @media ${device.small} {
+    font-size: 16px;
+    line-height: 20px;
+    letter-spacing: -1px;
+    width: 100%;
   }
 `
+
+const BackgroundTitleBig = styled.p`
+  font-size: 58px;
+
+  @media ${device.small} {
+    font-size: 22px;
+    line-height: 25px;
+    letter-spacing: -1px;
+  }
+`
+const BackgroundText = styled.article`
+  padding-top: 4rem;
+  font-size: 24px;
+  line-height: 32px;
+  width: 45%;
+
+  @media ${device.small} {
+    padding-top: 1.5rem;
+    width: 100%;
+    font-size: 16px;
+    line-height: 20px;
+    letter-spacing: -0.513925px;
+  }
+`
+
 const TextOnSecondaryColor = styled.span`
   color: ${globalStyles.secondaryColor};
-`
-
-const ExtraTitle = styled.div`
-  padding-top: 1rem;
-  opacity: 0.5;
-  grid-area: extra-title;
-  font-family: ${globalStyles.fontFamilyMedium};
-  line-height: 64px;
-  font-size: 64px;
-  color: ${globalStyles.inactiveColor};
-  padding-right: 20px;
-
-  @media ${device.phone} {
-    padding-top: 1rem;
-    padding-bottom: 2rem;
-    opacity: 0.4;
-    grid-area: extra-title;
-    font-family: ${globalStyles.fontFamilyMedium};
-    line-height: 32px;
-    font-size: 32px;
-    color: ${globalStyles.inactiveColor};
-  }
-`
-
-const ExtraText = styled.article`
-  padding-top: 1rem;
-  grid-area: extra-text;
-  font-family: ${globalStyles.fontFamilyRegular};
-  line-height: 32px;
-  font-size: 20px;
-
-  @media ${device.phone} {
-    padding-top: 0;
-  }
-`
-
-const BackgroundText = styled.article`
-  display: flex;
-  flex-direction: column;
-  grid-area: back-text;
-  font-family: ${globalStyles.fontFamilyRegular};
-  line-height: 32px;
-  font-size: 20px;
+  text-decoration: underline;
 `
 
 export default () => (
   <AboutWrapper id="about">
     <BackgroundTitle>
-      So <TextOnSecondaryColor>about me!</TextOnSecondaryColor>, what's my
-      background
+      <p>So about me...</p>
+      <BackgroundTitleBig>What's my background?</BackgroundTitleBig>
     </BackgroundTitle>
     <BackgroundText>
       <p>
-        Over the past 5 years, I have created a great variety of high quality
-        web business applications using technologies like C#, asp.net core,
-        HTML5, CSS3, Angular, Javascript and Typescript. I focus on creating
-        software that is testable, maintainable, reusable and adaptive,
-        requirements for success in Agile environments.
+        Over the past 5 years, I ‘ve created a great variety of high quality web
+        business applications using technologies like C#, asp.net core, HTML5,
+        CSS3, Angular, Javascript and Typescript. I focus on software that is
+        testable, maintainable, reusable and adaptive, requirements for success
+        in Agile environments.
       </p>
+      <br />
       <p>
-        I've have experience in startup, medium and large size enterprises and
-        I'm used to working in teams but also managing my own projects. Some of
-        the companies I've worked for include: Rydoo, Telefonica Argentina,
-        Cencosud and YPF. Check my
+        I've experience in startup, medium and large enterprises and I'm used to
+        working in teams... but also managing my own projects. Some of the
+        companies I've worked for include: Rydoo, Telefonica Argentina, Cencosud
+        and YPF.
+      </p>
+      <br />
+      <p>
+        As an employee I'm proactive, detail oriented and I enjoy playing an
+        active role in improving business.
+      </p>
+      <br />
+      <p>
+        Check my
         <a
           href="https://www.linkedin.com/in/patricio-ferraggi-ares/"
           target="_blank"
           rel="noopener noreferrer"
         >
           {" "}
-          LinkedIn{" "}
-        </a>
+          <TextOnSecondaryColor>LinkedIn</TextOnSecondaryColor>
+        </a>{" "}
         for more details.
       </p>
-      <p>
-        As an employee I'm proactive, detail oriented and I enjoy playing an
-        active role in improving business.
-      </p>
     </BackgroundText>
-
-    <ExtraTitle>Apart from work...</ExtraTitle>
-    <ExtraText>
-      <p>
-        I love writing on my blog, creating software development courses, giving
-        coaching to other developers and experimenting with multiple
-        technologies on my open source projects.
-      </p>
-      <p>
-        I exercise often and I've been practicing Muay Thai for over 7 years.
-      </p>
-      <p>
-        I also like to travel and taking pictures, specially from my cat who
-        hates me for it.
-      </p>
-    </ExtraText>
   </AboutWrapper>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Intro from "../components/landing/intro"
 import Layout from "../components/shared/layout"
 import Divider from "../components/shared/divider"
 import BlogNews from "../components/landing/blog-news/blog-news"
+import About from "../components/landing/about"
 
 export default () => (
   <Layout>
     <Intro></Intro>
     <Divider></Divider>
     <BlogNews></BlogNews>
+    <About></About>
   </Layout>
 )
