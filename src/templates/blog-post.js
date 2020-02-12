53c4351292b323ac75f119f0808da3470ba31128
Patricio Ferraggi <pattferraggi@gmail.com>
2020-02-12 17:44:46 +0100

    starting with blogpost, revived bio

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,10 +1,74 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import Layout from "../components/shared/layout"
-import SEO from "../components/shared/seo"
 import { Disqus, CommentCount } from "gatsby-plugin-disqus"
 import Img from "gatsby-image"
+import styled from "styled-components"
+import globalStyles from "../config/style-variables"
+import Layout from "../components/shared/layout"
+import SEO from "../components/shared/seo"
+import Divider from "../components/shared/divider"
+import Bio from "../components/blog/bio"
+
+const ArticleWrapper = styled.section`
+  font-family: ${globalStyles.fontFamilyMedium};
+  font-weight: 500;
+`
+
+const ArticleTitle = styled.h2`
+  font-size: 54px;
+  line-height: 64px;
+  letter-spacing: -1px;
+  width: 60%;
+  margin-bottom: 3rem;
+`
+
+const ArticleDate = styled.span`
+  font-size: 14px;
+  line-height: 32px;
+  opacity: 0.5;
+`
+
+const StyledCover = styled(Img)`
+  width: 90%;
+  margin: auto;
+  height: 475px;
+  margin-top: 100px;
+`
+
+const ArticleContent = styled.article`
+  background: linear-gradient(to bottom, #20202c 3%, #eaeae4 3%);
+  color: ${globalStyles.backgroundColor};
+`
+
+const ArticleContentText = styled.article`
+  padding: 3rem 17rem 6rem 17rem;
+  font-family: ${globalStyles.fontFamilyRegular};
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 32px;
+  letter-spacing: -0.5px;
+
+  p {
+    margin-bottom: 1em;
+  }
+
+  img,
+  pre {
+    display: block;
+    margin: 3.5rem auto 3.5rem auto;
+  }
+
+  hr {
+    margin-bottom: 2em;
+  }
+
+  a {
+    text-decoration: underline;
+    color: ${globalStyles.secondaryColor};
+  }
+`
 
 class BlogPostTemplate extends React.Component {
   componentDidMount() {
@@ -16,7 +80,6 @@ class BlogPostTemplate extends React.Component {
 
   render() {
     const post = this.props.data.mdx
-    const siteTitle = "El calabozo del desarrollador"
     const { previous, next } = this.props.pageContext
 
     let disqusConfig = {
@@ -25,48 +88,52 @@ class BlogPostTemplate extends React.Component {
     }
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-        />
-        <Img fixed={post.frontmatter.thumbnail.childImageSharp.fixed} />
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            display: `block`,
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <MDXRenderer>{post.body}</MDXRenderer>
-        <hr />
-        <CommentCount config={disqusConfig} />
-        <Disqus config={disqusConfig} />
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={`blog${previous.fields.slug}`} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={`blog${next.fields.slug}`} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
+      <Layout>
+        <ArticleWrapper>
+          <SEO
+            title={post.frontmatter.title}
+            description={post.frontmatter.description || post.excerpt}
+          />
+          <ArticleTitle>{post.frontmatter.title}</ArticleTitle>
+          <ArticleDate>{post.frontmatter.date}</ArticleDate>
+          <Divider></Divider>
+          <ArticleContent>
+            <StyledCover
+              fluid={post.frontmatter.thumbnail.childImageSharp.fluid}
+            />
+            <ArticleContentText>
+              <MDXRenderer>{post.body}</MDXRenderer>
+              <hr />
+              <ul
+                style={{
+                  display: `flex`,
+                  flexWrap: `wrap`,
+                  justifyContent: `space-between`,
+                  listStyle: `none`,
+                  padding: 0,
+                }}
+              >
+                <li>
+                  {previous && (
+                    <Link to={`blog${previous.fields.slug}`} rel="prev">
+                      ← {previous.frontmatter.title}
+                    </Link>
+                  )}
+                </li>
+                <li>
+                  {next && (
+                    <Link to={`blog${next.fields.slug}`} rel="next">
+                      {next.frontmatter.title} →
+                    </Link>
+                  )}
+                </li>
+              </ul>
+              <Bio></Bio>
+            </ArticleContentText>
+          </ArticleContent>
+          <CommentCount config={disqusConfig} />
+          <Disqus config={disqusConfig} />
+        </ArticleWrapper>
       </Layout>
     )
   }
@@ -95,8 +162,8 @@ export const pageQuery = graphql`
         description
         thumbnail {
           childImageSharp {
-            fixed(width: 300, height: 300) {
-              ...GatsbyImageSharpFixed
+            fluid {
+              src
             }
           }
         }
