dc09d015af4d0e1b4e7a944fbeaf6716cdeceb62
Patricio Ferraggi <pattferraggi@gmail.com>
2020-03-23 21:38:49 +0100

    create rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,79 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-feed-mdx`,
     {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  title:
+                    edge.node.frontmatter.title +
+                    (edge.node.frontmatter.description
+                      ? ` - ${edge.node.frontmatter.description}`
+                      : ``),
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  enclosure: {
+                    url:
+                      site.siteMetadata.siteUrl +
+                      edge.node.frontmatter.thumbnail.childImageSharp.fluid.src,
+                  },
+                  url:
+                    site.siteMetadata.siteUrl + `/blog` + edge.node.fields.slug,
+                  guid:
+                    site.siteMetadata.siteUrl + `/blog` + edge.node.fields.slug,
+                  // custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date(formatString: "DD MMMM YYYY", locale: "es")
+                        description
+                        thumbnail {
+                          childImageSharp {
+                            fluid {
+                              src
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "patferraggi.dev RSS Feed",
+          },
+        ],
+      },
+    },
+    {
       resolve: "gatsby-plugin-mailchimp",
       options: {
         endpoint:
